e361d39510bacf7191add8192059610181ee77b0
Faisal7007 <126740052+Faisal7007@users.noreply.github.com>
2023-08-31 14:32:53 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import './App.css';
+import Main from './components/main/Main';
+import All from './components/all/All';
+import Cart from './components/cart/Cart';
+import Shopping from './components/shopping/Shopping';
+import Study from './components/study/Study';
+import Travel from './components/travel/Travel';
+import Work from './components/work/Work';
+import Create from './components/create/Create';
+import Home from './components/home/Home';
+import Task from './components/task/Task';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app_inner">
+        <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='/All' element={<All/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/Shopping' element={<Shopping/>}/>
+          <Route path='/Study' element={<Study/>}/>
+          <Route path='/Travel' element={<Travel/>}/>
+          <Route path='/Work' element={<Work/>}/>
+          <Route path='/create' element={<Create/>}/>
+          <Route path='/Home' element={<Home/>}/>
+          <Route path='/task' element={<Task/>}/>
+        </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,51 @@
+
+
+const data = [
+    {
+        id:'1',
+        logo:null,
+        name:'All',
+        task:null,
+        link:'/all'
+    },
+    {
+        id:'2',
+        logo:null,
+        name:'Work',
+        task:null,
+        link:'/work'
+
+    },
+    {
+        id:'3',
+        logo:null,
+        name:'Study',
+        task:null,
+        link:'/study'
+
+    },
+    {
+        id:'4',
+        logo:null,
+        name:'Travel',
+        task:null,
+        link:'/travel'
+
+    },
+    {
+        id:'5',
+        logo:null,
+        name:'Shopping',
+        task:null,
+        link:'/shopping'
+
+    },
+    {
+        id:'6',
+        logo:null,
+        name:'Home',
+        task:null,
+        link:'/home'
+    }
+]
+export default data
